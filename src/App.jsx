eb81bd8bc470a8d813e0a1bce6c2dfb4e0676778
Andokely <andoratovo12@gmail.com>
2024-04-12 15:02:27 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='flex justify-center items-center h-screen'>
+        <div className='bg-red-300 rounded-lg p-5 items-center w-full'>
+          <h1 className='uppercase font-bold'>Hello world</h1>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
