f1d2ac89df38ed6c823b19da914575c53951eb7d
Andokely <andoratovo12@gmail.com>
2024-04-13 00:21:52 +0300

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,58 @@
-import axios from "axios";
 import React, { useState } from "react";
-import ip from "../../ip.json";
-import { ToastContainer, toast } from "react-toastify";
-import "react-toastify/dist/ReactToastify.css";
 
-export const Login = () => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
+export const App = () => {
 
   return (
-    <div>
-      <form
-        action=""
-        onSubmit={handleSubmit}
-        className="flex flex-col space-y-4"
-      >
-        <label htmlFor="username" className="text-gray-600">
-          Nom d'utilisateur :
-        </label>
-        <input
-          type="text"
-          name="username"
-          value={username}
-          className="px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          autoComplete="off"
-          placeholder="Nom d'utilisateur"
-        />
-        <label htmlFor="password" className="text-gray-600">
-          Mot de passe :
-        </label>
-        <input
-          type="password"
-          name="password"
-          value={password}
-          className="px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
-          autoComplete="off"
-          placeholder="Mot de passe"
-        />
-        <button
-          type="submit"
-          className="px-4 py-2 text-white bg-[#00416A] rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
-        >
-          Login
-        </button>
-      </form>
+    <div className="flex items-center bg-slate-600 justify-center h-screen">
+      <div className="p-8 bg-white rounded-lg shadow-md w-96 shadow-gray-400">
+        <h2 className="text-2xl text-blue-600 text-center font-bold">
+          Se connecter
+        </h2>
+        <hr className='my-3 h-[2px] rounded-xl' />
+        <div>
+          <form
+            action=""
+            className="flex flex-col space-y-4"
+          >
+            <label htmlFor="username" className="text-gray-600">
+              Nom d'utilisateur :
+            </label>
+            <input
+              type="text"
+              name="username"
+              className="px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+              placeholder="Nom d'utilisateur"
+            />
+            <label htmlFor="password" className="text-gray-600">
+              Mot de passe :
+            </label>
+            <input
+              type="password"
+              name="password"
+              className="px-3 py-2 border rounded-md focus:outline-none focus:border-blue-500"
+              autoComplete="off"
+              placeholder="Mot de passe"
+            />
+            <button
+              type="submit"
+              className="px-4 py-2 text-white bg-red-600 rounded-md hover:bg-blue-600 focus:outline-none focus:ring focus:border-blue-300"
+            >
+              Login
+            </button>
+          </form>
+        </div>
+        <div className="mt-4">
+          <button
+            className="text-blue-500 hover:underline focus:outline-none"
+          >
+            Vous n' avez pas encore un compte ? Créer ici
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
+
+export default App;
+
